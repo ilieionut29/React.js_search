26ba363400a38e15c206bffd7fab10b2b068530b
Ionut Ilie <ionut@leadlion.ro>
2021-08-03 12:55:48 +0300

    upload files

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Nunc tempus, justo non varius euismod, dolor neque imperdiet arcu, in
+        consectetur tellus odio ac augue. Sed feugiat rhoncus odio cursus
+        vestibulum. Aenean ac vestibulum sapien. Vestibulum rutrum, arcu ac
+        sagittis consequat, sem nisi aliquam lectus, eu rutrum lorem nunc in
+        lorem. Curabitur venenatis, nulla id varius tincidunt, quam turpis
+        rutrum velit, non posuere metus ex ac nulla. Maecenas pellentesque
+        luctus leo, ac vestibulum lacus scelerisque sit amet. Duis eget egestas
+        nunc. Etiam pretium suscipit facilisis. Fusce volutpat, risus vitae
+        sodales elementum, massa sem rutrum sem, in gravida magna justo vitae
+        diam. Phasellus nibh purus, scelerisque id iaculis non, finibus vitae
+        ante. Class aptent taciti sociosqu ad litora torquent per conubia
+        nostra, per inceptos himenaeos. Vestibulum facilisis iaculis risus non
+        feugiat. Duis suscipit lacus at risus tempus congue.
+      </p>
+    </section>
+  );
+};
+
+export default About;
